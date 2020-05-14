390c7a988c932f5b181625720d64d2c85f9cd5af
srinivas-chatnalli <58304114+srinivas-chatnalli@users.noreply.github.com>
2020-05-14 10:43:11 +0530

    Update settings.py

--- sampledeploy/sampledeploy/settings.py
+++ sampledeploy/sampledeploy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'protected-citadel-84025.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'immense-coast-21229.herokuapp.com']
 
 # Application definition
 
